47731d091f39614dab8a1ed8064948f20b14d103
prajwel <prajwel.pj@gmail.com>
2026-06-23 12:01:14 +0530

    process_events_list included in API.

--- src/resolvit/__init__.py
+++ src/resolvit/__init__.py
@@ -1,6 +1,11 @@
-from .resolvit import process_observation, __version__
+from .resolvit import (
+    process_observation,
+    process_events_list,
+    __version__,
+)
 
 __all__ = [
     "process_observation",
+    "process_events_list",
     "__version__",
 ]
